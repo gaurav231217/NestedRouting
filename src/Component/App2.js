bb8d8f6249b3e8e29861ebf04df47a2406044fe4
=Chandan <='chandrab@observancegroup.com>
2025-07-02 15:08:28 +0530

    gkt

--- src/Component/App2.js
+++ src/Component/App2.js
@@ -1,25 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar2 from "./Navbar2";
 import OTP from "./OTP";
 import { Outlet, Routes, Route,Navigate } from "react-router-dom";
 import HomePage from "./HomePage";
+import "./assest/style.css";
+import { useState } from "react";
 
 
 function App2() {
+  useEffect(()=>{
+    document.getElementsByClassName("tabvalue")[1].style.display="none";
+  });
+  function tab(index){
+    
+    if(index==0){
+      document.getElementsByClassName("tabvalue")[index].style.display="block";
+       document.getElementsByClassName("tabvalue")[index+1].style.display="none";
+
+    }
+    else{
+            document.getElementsByClassName("tabvalue")[index-1].style.display="none";
+       document.getElementsByClassName("tabvalue")[index].style.display="block";
+    }
+  }
   return (
-    <>
-      <Routes>
-         
-        <Route path="/" element={<Navbar2 />}>
-          <Route index element={<HomePage/>} />
-         
-          <Route path="gkt" element={<OTP />} />
-         
-          {/* Add more routes under /otp/ as needed */}
-        </Route>
-      </Routes>
-      <Outlet/>
-    </>
+           
+       <div className="container">
+        <div className="tab">
+          <div className="tabs" onClick={()=>{tab(0)}}>Home</div>
+          <div className="tabs" onClick={()=>{tab(1)}}>Otp</div>
+          <div className="tabs">Bombay</div>
+        </div>
+        <div className="tabrender">
+          <div className="tabvalue"><HomePage/></div>
+          <div className="tabvalue"><OTP/></div>
+          
+        </div>
+       </div>
+
   );
 }
 
--- src/Component/HomePage.js
+++ src/Component/HomePage.js
@@ -1,13 +1,18 @@
-
-
-
 import React from "react";
+import "./assest/style.css";
 
 function HomePage() {
   return (
-    <div className="p-8 text-center  min-h-screen">
-      <h1 className="text-3xl font-bold mb-4">Welcome to Home Page</h1>
-      <p>This is the homepage content.</p>
+    <div className="homepage-container">
+      <h1>Welcome to Our Application</h1>
+      <p className="homepage-subtext">
+        This is a sample homepage built with React. You can navigate through tabs
+        to explore different features like OTP generation, form submissions,
+        user profiles, and more.
+      </p>
+      <button className="homepage-button" onClick={() => alert("Explore clicked!")}>
+        Explore Now
+      </button>
     </div>
   );
 }
